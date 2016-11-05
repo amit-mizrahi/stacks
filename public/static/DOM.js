6566490384398509be41e2c4807f50c797d60d49
Amit Mizrahi <amit@amizrahi.com>
2016-11-05 07:40:20 -0400

    DOM.js

---
+++ public/static/DOM.js
@@ -0,0 +1,24 @@
+// requires Config.js
+
+var DOM = {
+  updateScoreText: function(score) {
+    $(".score-display").text(score);
+  },
+
+  showGameOverText: function() {
+    $("#title").hide();
+    $("#explanation").hide();
+    $("#game-over-heading").show();
+    $("#game-over-text").show();
+  },
+
+  hideGameOverText: function() {
+    $("#title").show();
+    $("#game-over-heading").hide();
+    $("#game-over-text").hide();
+  },
+
+  fillRect: function(ctx, x, y, width, height) {
+    ctx.fillRect(x, Geometry.CANVAS_HEIGHT - y, width, -1 * height);
+  }
+}
